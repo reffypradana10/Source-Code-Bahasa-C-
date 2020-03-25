787dd9658d3ef1b98a4e85a56c15ecc8f1e0b9c0
Refi Yusuf Pradana <58463207+reffypradana10@users.noreply.github.com>
2020-03-25 23:11:40 +0700

    Add files via upload

---
+++ web petanikode/Belajar C++ #03/main.cpp
@@ -0,0 +1,91 @@
+ //* Struktur Dasar Program C++ *//
+ /*
+ // Bagian deklarasi Include
+ #include <iostream>
+
+// Bagian deklarasi namespace
+using namespace std;
+
+// Bagian deklarasi fungsi
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+*/
+
+//* Contoh Fungsi *//
+/*
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
+
+void hello()
+{
+    cout << "Hello Apa Kabar" << endl;
+}
+*/
+
+//* Contoh Penulisan Statement *//
+/*
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "Hari ini saya belajar C++" << endl;
+    cout << "Belajar C++ itu mudah";
+    return 0;
+}
+*/
+
+//* Contoh Penulisan WhiteSpace gak karuan *//
+/*
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout <<         "HELLO WORLD" << endl;
+    return                          0;
+}
+*/
+
+//* Contoh Penulisan String dan Karakter *//
+/*
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // contoh penulisan string
+    cout << "Tutorial C++ untuk pemula" << endl;
+    // contoh penulisan karakter
+    cout << 'a' << endl;
+}
+*/
+
+//* Contoh Penulisan Komentar *//
+
+#include <iostream>
+
+using namespace std;
+
+/*ini adalah komentar
+yang lebih dari satu baris*/
+
+int main()
+{
+    // ini adalah komentar satu baris
+    cout << "hello world" << endl;
+    return 0;
+}
